cb2ee27523b6731ca9ce22cc4d5f04454f2efa9e
obidul <obaidulhoqejoy@gmail.com>
2025-04-10 16:04:41 +0600

    init commit

---
+++ src/test/java/com/storedata/Info_ofEmp/InfoOfEmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storedata.Info_ofEmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoOfEmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
